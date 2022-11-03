1b9e10a47b5482a78e33a7fa56fc6042a1c22bcf
SamuilDanchev <113180851+SamuilDanchev@users.noreply.github.com>
2022-11-03 10:26:39 +0100

    8 files modified, 7 added

---
+++ DigitalStudentIDCard/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Router } from '@angular/router';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  @Input() error: string | undefined;
+
+  @Output() submitEM = new EventEmitter();
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+      this.router.navigate(['/student']);
+    }
+  }
+
+
+}
